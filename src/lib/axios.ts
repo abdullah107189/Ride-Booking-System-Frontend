b661d77a1cc8023dcff6c96dc99e520e9ea71d6d
abdullah107189 <abdullah107189@gmail.com>
2025-10-21 22:59:58 +0600

    set axiso with creadential

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: config.baseURL,
+  withCredentials: true,
 });
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
